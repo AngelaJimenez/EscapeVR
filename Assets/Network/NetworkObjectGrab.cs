5fb62237e2fb5eb8cddffe6a3680a24332de2150
AngelaJimenez <60157012+Af-Rincon@users.noreply.github.com>
2022-09-12 16:52:47 -0500

    Photon Network

---
+++ Assets/Network/NetworkObjectGrab.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+using Photon.Pun;
+
+public class NetworkObjectGrab : XRGrabInteractable
+{
+    private PhotonView photonView;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        photonView = GetComponent<PhotonView>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected override void OnSelectEntering(XRBaseInteractor interactor)
+    {
+        photonView.RequestOwnership();
+        base.OnSelectEntering(interactor);
+    }
+}
