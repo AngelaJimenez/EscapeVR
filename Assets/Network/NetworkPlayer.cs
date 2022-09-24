9b53d4e2c3a19eeeaa8f28bdd699cac75216dafb
AngelaJimenez <49964187+AngelaJimenez@users.noreply.github.com>
2022-09-23 22:32:54 -0500

    online

--- Assets/Network/NetworkPlayer.cs
+++ Assets/Network/NetworkPlayer.cs
@@ -4,36 +4,65 @@ using UnityEngine;
 using UnityEngine.XR;
 using Photon.Pun;
 using UnityEngine.XR.Interaction.Toolkit;
-
+using Unity.XR.CoreUtils;
+using UnityEngine;
+using UnityEngine.XR;
 public class NetworkPlayer : MonoBehaviour
 {
     public Transform head;
     public Transform leftHand;
     public Transform rightHand;
     private PhotonView photonView;
+    public Animator rightHandAnimator;
+    public Animator leftHandAnimator;
 
-    public Transform headRig;
-    public Transform leftHandRig;
-    public Transform rightHandRig;    
+    private Transform headRig;
+    private Transform leftHandRig;
+    private Transform rightHandRig;    
 
     // Start is called before the first frame update
     void Start()
     {
         photonView = GetComponent<PhotonView>();
-        XRRig rig = FindObjectOfType<XRRig>();
+        XROrigin rig = FindObjectOfType<XROrigin>();
+       
         headRig = rig.transform.Find("Camera Offset/Main Camera");
         leftHandRig = rig.transform.Find("Camera Offset/LeftHand Controller");
         rightHandRig = rig.transform.Find("Camera Offset/RightHand Controller");
+        if(photonView.IsMine)
+        {
+            foreach (var item in GetComponentsInChildren<Renderer>())
+            {
+                item.enabled=false;
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(photonView.IsMine)
+         
+        if(headRig== null)
         {
-            rightHand.gameObject.SetActive(false);
-            leftHand.gameObject.SetActive(false);
-            head.gameObject.SetActive(false);
+            XROrigin rig = FindObjectOfType<XROrigin>();
+            headRig = rig.transform.Find("Camera Offset/Main Camera");
+        }
+        if(leftHandRig== null)
+        {
+            XROrigin rig = FindObjectOfType<XROrigin>();
+            leftHandRig = rig.transform.Find("Camera Offset/LeftHand Controller");
+        }
+        if(rightHandRig== null)
+        { XROrigin rig = FindObjectOfType<XROrigin>();
+            rightHandRig = rig.transform.Find("Camera Offset/RightHand Controller");
+        }
+        if(photonView.IsMine)
+        {   
+            UpdateHandAnimation(InputDevices.GetDeviceAtXRNode(XRNode.LeftHand), leftHandAnimator);
+            UpdateHandAnimation(InputDevices.GetDeviceAtXRNode(XRNode.RightHand), rightHandAnimator);
+            // rightHand.gameObject.SetActive(false);
+            // leftHand.gameObject.SetActive(false);
+            // head.gameObject.SetActive(false);
 
             MapPosition(head, headRig);
             MapPosition(leftHand, leftHandRig);
@@ -41,7 +70,26 @@ public class NetworkPlayer : MonoBehaviour
         }
         
     }
+void UpdateHandAnimation(InputDevice targetDevice, Animator handAnimator)
+    {
+        if(targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue))
+        {
+            handAnimator.SetFloat("Trigger", triggerValue);
+        }
+        else
+        {
+            handAnimator.SetFloat("Trigger", 0);
+        }
 
+        if (targetDevice.TryGetFeatureValue(CommonUsages.grip, out float gripValue))
+        {
+            handAnimator.SetFloat("Grip", gripValue);
+        }
+        else
+        {
+            handAnimator.SetFloat("Grip", 0);
+        }
+    }
     void MapPosition(Transform target, Transform rigTransform)
     {
         target.position = rigTransform.position;
