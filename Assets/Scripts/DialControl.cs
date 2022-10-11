50738923bd4074c7b127b77ab898ffe2e359992a
Af-Rincon <af.rincon@uniandes.edu.co>
2022-10-11 12:48:13 -0500

    script candado

--- Assets/Scripts/DialControl.cs
+++ Assets/Scripts/DialControl.cs
@@ -4,17 +4,41 @@ using UnityEngine;
 
 public class DialControl : MonoBehaviour
 {
-    private float posicion;
+    public GameObject body;
+    public GameObject dial1;
+    public GameObject dial2;
+    public GameObject dial3;
+    public GameObject dial4;
+
+    private Animator animator;
+
+    private bool cambio = false;
+
+    public char[] clave = new char[4];
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        this.transform.localRotation = new Quaternion(0,0,0,0);
+        animator = body.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        posicion = this.transform.localRotation.z;
+        Debug.Log(dial1.transform.localRotation.z);
+        if(dial1.transform.localRotation.z > -0.1f && dial1.transform.localRotation.z < 0.1f
+            && dial2.transform.localRotation.z > 0.2f && dial2.transform.localRotation.z < 0.4f
+            && dial3.transform.localRotation.z > 0.5f && dial3.transform.localRotation.z < 0.7f
+             && dial4.transform.localRotation.z > 0.8f && dial4.transform.localRotation.z < 1.0f
+             && cambio ==false)
+        {
+            Debug.Log("correcto");
+            cambio = true;
+            animator.SetBool("Open", true);
+            
+        }
+       
     }
 }
