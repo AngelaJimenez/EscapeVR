7ca996559490ad474e9c85babd165baa5e8c235a
AngelaJimenez <49964187+AngelaJimenez@users.noreply.github.com>
2022-10-14 08:50:36 -0500

    clock solution (first try)

--- Assets/Scripts/gearatach.cs
+++ Assets/Scripts/gearatach.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 public class gearatach : MonoBehaviour
 {
-        private bool isKey=false;
         public string taggear;
         private GameObject foundIt;
         public GameObject keyHolder;
         public GameObject me;
         public Vector3 position =  new Vector3(-13.063f,0.989f, -10.483f);
         public GameObject pointer;
+        public bool positioned=false;
 
  void OnCollisionEnter(Collision collision)
     {
@@ -23,8 +23,7 @@ public class gearatach : MonoBehaviour
  public void check() {
 
        
-        isKey = true;
-        if (foundIt )
+        if (foundIt && !positioned)
     {
            
          Rigidbody m_Rigidbody= foundIt.GetComponent< Rigidbody>();
@@ -36,7 +35,26 @@ public class gearatach : MonoBehaviour
         me.SetActive(false);
         Gearmove script= foundIt.AddComponent<Gearmove>();
         script.pointer= pointer;
+        positioned=true;
     }
 }
+ 
+  void Update() {
+ if (foundIt && !positioned)
+    {
+           
+         Rigidbody m_Rigidbody= foundIt.GetComponent< Rigidbody>();
+         foundIt.transform.position=position;
+         m_Rigidbody.useGravity = false;
+         foundIt.transform.rotation= Quaternion.Euler(new Vector3(0, 180, 0));
+            m_Rigidbody.constraints = RigidbodyConstraints.FreezePositionZ |  RigidbodyConstraints.FreezePositionY | RigidbodyConstraints.FreezePositionX |RigidbodyConstraints.FreezeRotationY|RigidbodyConstraints.FreezeRotationX;
+        keyHolder.SetActive(false);
+        me.SetActive(false);
+        Gearmove script= foundIt.AddComponent<Gearmove>();
+        script.pointer= pointer;
+        positioned=true;
+    }   
+ }
+
   
 }
