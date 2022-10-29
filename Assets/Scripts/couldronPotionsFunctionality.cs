e60f30310f5b1b62e8eee56b5965d8ce60ecb5fc
AngelaJimenez1 <60157012+Af-Rincon@users.noreply.github.com>
2022-10-29 18:34:50 -0500

    pociones

--- Assets/Scripts/couldronPotionsFunctionality.cs
+++ Assets/Scripts/couldronPotionsFunctionality.cs
@@ -2,12 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class couldronPotionsFunctionality : MonoBehaviour
 {
+
+    private int[] cafe;
+    private int[] chocolate;
+    private int[] leche;
+    private int[] azucar;
+    private int[] crema;
+    
+    private int[] final;
+
+    private int[] mezcla;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cafe = new int[]{1,1,0,0,1,0,0,0,0,0,0};
+        chocolate = new int[]{1,0,1,1,0,0,0,0,0,0,0};
+        leche = new int[]{0,1,0,1,0,1,0,0,0,0,0};
+        azucar = new int[]{0,0,1,0,1,0,0,0,0,0,0};
+        crema = new int[]{0,0,0,0,0,0,0,0,1,1,0};
+        final = new int[]{0,0,0,0,0,0,1,1,1,1,1};
     }
 
     // Update is called once per frame
@@ -16,12 +33,50 @@ public class couldronPotionsFunctionality : MonoBehaviour
         
     }
 
-    
     void OnTriggerEnter(Collider other) {
        if(other.gameObject.tag== "potion")
        {
+        string pocion = other.gameObject.name;
         Debug.Log(other.gameObject.name);
+
+        if(pocion == "E")
+        {
+            mezcla[0] += 1;
+        }
+          if(pocion == "G")
+        {
+            mezcla[1] += 1;
+        }
+          if(pocion == "H")
+        {
+            mezcla[2] += 1;
+        }
+          if(pocion == "J")
+        {
+            mezcla[3] += 1;
+        }
+          if(pocion == "K")
+        {
+            mezcla[4] += 1;
+        }
+          if(pocion == "L")
+        {
+            mezcla[5] += 1;
+        }
+
+       
        }
+    }
+
+    void CheckMix()
+    {
+        for(int i = 0;i<mezcla.Length;i++)
+        {
+            if(mezcla==cafe|| mezcla == chocolate || mezcla == azucar || mezcla == leche)
+            {
+                
+            }
+        }
     }
 
 }
